ac4202b70b1b60bad1a984a871329342eef709f1
Libin <libinabraham1993@gmail.com>
2025-05-17 23:14:21 +0100

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,14 @@
 function ListGroup() {
-  const items = ["1", "2", "3", "4", "5"];
+  let items = ["1", "2", "3", "4", "5"];
+
+  items = [];
+
+  const message = items.length === 0 && <p>No item found</p>;
 
   return (
     <>
       <h1>List</h1>
+      {message}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
