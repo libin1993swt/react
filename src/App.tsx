6ea6407be8c3d3e8bd41b336001df1a0c16bf690
Libin <libinabraham1993@gmail.com>
2025-05-18 18:53:37 +0100

    Heading updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ function App() {
   let items = ["1", "2", "3", "4", "5"];
   return (
     <div>
-      <ListGroup items={items} heading="cities" />
-      <ListGroup items={items} heading="cities" />
+      <ListGroup items={items} heading="Cities" />
+      <ListGroup items={items} heading="Cities" />
     </div>
   );
 }
